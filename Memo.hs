b08601266b80d23cc2b0d4fe38159004434dfbc5
Chris Wong <lambda.fairy@gmail.com>
2016-02-24 17:36:50 +1300

    Split code into two modules

---
+++ Memo.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+
+module Memo (
+    memo,
+    memoFix,
+    ) where
+
+
+import Prelude hiding (lookup)
+
+import Core
+
+
+-- | A lazy, infinite binary tree which allows for @O(log n)@ indexing
+-- at all points.
+data Tree a = Branch a (Tree a) (Tree a)
+    deriving Functor
+
+
+-- | Look up a value in the tree.
+lookup :: Natural -> Tree a -> a
+lookup n (Branch value left right)
+    | n == 0 = value
+    | r == 0 = lookup q left
+    | otherwise = lookup q right
+  where
+    (q, r) = pred n `quotRem` 2
+
+
+-- | A tree containing all the natural numbers such that the value at
+-- @n@ is precisely @n@.
+naturals :: Tree Natural
+naturals = Branch 0
+    (succ . (2 *) <$> naturals)
+    (succ . succ . (2 *) <$> naturals)
+
+
+-- | Memoize a sequence function.
+memo :: Sequence a -> Sequence a
+memo f = \n -> lookup n tree
+  where
+    tree = f <$> naturals
+
+
+-- | Memoize a recursive sequence function.
+memoFix :: (Sequence a -> Sequence a) -> Sequence a
+memoFix ff = f
+  where
+    f = memo (ff f)
