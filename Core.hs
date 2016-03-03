f22a20b1900d2d806e7d051ecd43454377ea6d0b
Chris Wong <lambda.fairy@gmail.com>
2016-03-03 22:23:08 +1300

    Add modulus of Cauchy convergence

--- Core.hs
+++ Core.hs
@@ -15,3 +15,21 @@ instance Num Cauchy where
     abs (Cauchy f) = Cauchy (\n -> abs (f n))
     signum (Cauchy f) = Cauchy (\n -> signum (f n))
     fromInteger n = Cauchy (\_ -> fromInteger n)
+
+
+-- | Cauchy sequence with a modulus of convergence.
+--
+-- The second argument is a /modulus function/, @mu :: N -> N@, which
+-- for any @k@ gives the least index where the difference is less than
+-- @2^(-mu k)@.
+data Cauchy' = Cauchy' Cauchy (Natural -> Natural)
+
+
+instance Num Cauchy' where
+    Cauchy' f u + Cauchy' g v = Cauchy' (f + g) (\k -> max (u (1+k)) (v (1+k)))
+    Cauchy' f u - Cauchy' g v = Cauchy' (f - g) (\k -> max (u (1+k)) (v (1+k)))
+    (*) = error "(*) not implemented"
+    negate (Cauchy' f u) = Cauchy' (negate f) u
+    abs (Cauchy' f u) = Cauchy' (abs f) u
+    signum (Cauchy' f u) = Cauchy' (signum f) u
+    fromInteger n = Cauchy' (fromInteger n) (\_ -> 0)
