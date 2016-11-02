37f5bcc9063c5504e2f75463ddf18325345da485
Chris Wong <lambda.fairy@gmail.com>
2016-11-02 21:03:59 +1300

    Add example code

---
+++ demo.hs
@@ -0,0 +1,17 @@
+:l Cauchy.hs CalkinWilf.hs
+
+1 + 1 :: Cauchy
+
+let det (a, b, c, d) = a * d - b * c
+
+det (64919121, -159018721, 41869520.5, -102558961) :: Double
+
+det (64919121, -159018721, 41869520.5, -102558961) :: Cauchy
+
+expC 1
+
+exp 1
+
+printToPrecision 100 $ expC 1
+
+printToPrecision 100 $ expC (-7) / sinC 9 + cosC (1/3) * 42.42
